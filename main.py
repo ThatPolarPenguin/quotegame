cb67553c4f91d77dc3c47717fac7f39ca24ec5a6
Karch Forward <kforward865@gmail.com>
2023-09-17 15:57:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,10 @@
 # list and begins to ask the player what they would like to do via the game menu
 
 import game
-import shop
 from shop import Thrawn
 
 if __name__ == "__main__":
     
-    Thrawn.add_credits(1000)
     game.start_game()
     
 
